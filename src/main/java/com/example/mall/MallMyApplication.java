d58f80903434a4e8f3055fd398b9e752b33861c2
1612587615@qq.com <20000823lyb520>
2020-09-23 15:48:53 +0800

    初始化

---
+++ src/main/java/com/example/mall/MallMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mall/MallMyApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.mall;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MallMyApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
